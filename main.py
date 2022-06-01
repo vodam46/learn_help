887a58d3baf71e661e2f77edf3d4b4899d06910f
vodam46 <adamnuc@email.cz>
2022-06-01 21:09:07 +0200

    german articles, and generalized main script

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 import json
 import random
+from sys import argv
 
-values = json.loads(open("letopocty.json").read())
+if len(argv) == 2:
+    values = json.loads(open(argv[1]).read())
+else:
+    values = json.loads(open(input("name of file: ")).read())
+
 while True:
-    question, answer = random.choice(list(values[random.choice(["svet", "cr"])].items()))
-    print(question)
+    category = random.choice(list(values.keys()))
+    question, answer = random.choice(list(values[category].items()))
+    print(category + ": " + question)
     player_answer = input()
-    if player_answer == answer:
+    if player_answer == "q":
+        quit()
+    elif player_answer == answer:
         print("spravne")
     elif player_answer == "":
         print(answer)
