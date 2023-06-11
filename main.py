b1ad1c478025430e4bd742ae966e897892ca9e1a
vodam46 <adamnuc@email.cz>
2023-06-11 15:28:58 +0200

    german prepositions, diacritics in letopocty.json, changed choosing to shuffle

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
+#!/usr/bin/env python3
 import json
 import random
 from sys import argv
+import itertools
 
 if len(argv) == 2:
     values = json.loads(open(argv[1]).read())
 else:
     values = json.loads(open(input("name of file: ")).read())
 
+values = list(itertools.chain.from_iterable([[[
+            category+": "+question, answer
+            ] for question, answer in list(values[category].items())
+    ] for category in list(values.keys())]))
+
 while True:
-    category = random.choice(list(values.keys()))
-    question, answer = random.choice(list(values[category].items()))
-    print(category + ": " + question)
-    player_answer = input()
-    if player_answer == "q":
-        quit()
-    elif player_answer == answer:
-        print("spravne")
-    elif player_answer == "":
-        print(answer)
-    else:
-        print("spatne: ", answer)
+    for question, answer in random.sample(values, len(values)):
+        check_answer = answer.replace(" ", "")
+        print(question)
+        player_answer = input().replace(" ", "")
+        if player_answer == "q":
+            quit()
+        elif player_answer == check_answer:
+            print("✅ " + answer)
+        elif player_answer == "":
+            print(answer)
+        else:
+            print("❌ ", answer)
