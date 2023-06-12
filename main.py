94f5fac7f2ac02b6aa9e499626c55db8ff48253b
vodam46 <adamnuc@email.cz>
2023-06-12 21:53:43 +0200

    minor grammar in letopocty.json, results printing

--- main.py
+++ main.py
@@ -14,16 +14,25 @@ values = list(itertools.chain.from_iterable([[[
             ] for question, answer in list(values[category].items())
     ] for category in list(values.keys())]))
 
+
 while True:
+    correct = 0
+    wrong = 0
+    asked = 0
     for question, answer in random.sample(values, len(values)):
         check_answer = answer.replace(" ", "")
         print(question)
         player_answer = input().replace(" ", "")
         if player_answer == "q":
+            print(f"✅ {correct} ❌ {wrong} ❓ {asked}")
             quit()
         elif player_answer == check_answer:
+            correct += 1
             print("✅ " + answer)
         elif player_answer == "":
+            asked += 1
             print(answer)
         else:
+            wrong += 1
             print("❌ ", answer)
+    print(f"✅ {correct} ❌ {wrong} ❓ {asked}")
